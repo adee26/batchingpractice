ec32d857870c76712b7b741b425c55b7eea086e0
Adelina Apetrei <adelina.apetrei26@gmail.com>
2024-01-29 12:36:42 +0200

    Optimize imports

--- src/main/java/com/adedev/batchingpractice/BatchingPracticeApplication.java
+++ src/main/java/com/adedev/batchingpractice/BatchingPracticeApplication.java
@@ -1,10 +1,7 @@
 package com.adedev.batchingpractice;
 
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
